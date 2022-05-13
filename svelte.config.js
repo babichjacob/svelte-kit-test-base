d7a51ab1ebb61ad6539b690670ace4c6ca4ef314
J / Jacob Babich <jacobbabichpublic+git@gmail.com>
2022-05-13 15:20:02 -0400

    use the base path!!!

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,38 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
 
+import { mkdir, readdir, rename, } from "fs/promises";
+
+const build = "build";
+const base = "/blog/me";
+const output = `${build}${base}`;
+const root = base.split("/")[1];
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: "404.html",
-    }),
+    adapter: {
+      name: "custom-static-adapter",
+      adapt: async (utils) => {
+        await adapter({
+          assets: build,
+          pages: build,
+          fallback: "404.html",
+        }).adapt(utils);
+
+        await mkdir(output, {
+          recursive: true,
+        });
+
+        for (const file of await readdir(build)) {
+          if (file === root) continue;
+          await rename(`${build}/${file}`, `${output}/${file}`);
+        }
+
+      },
+    },
     paths: {
-      base: "/blog/me",
+      base,
     },
 
     trailingSlash: "always",
